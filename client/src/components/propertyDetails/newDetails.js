f7393051df9bd24661680e66f96b132f30fecd14
leer <ladesigns90@gmail.com>
2020-03-13 12:44:27 -0400

    created all propertyDetail files to allow create, edit, delete functionality.

---
+++ client/src/components/propertyDetails/newDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class newDetails extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
