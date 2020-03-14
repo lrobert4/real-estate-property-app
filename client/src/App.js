cd319f5adcb36475c1ce69ba2f7e4f8f95bfc406
leer <ladesigns90@gmail.com>
2020-03-14 00:04:10 -0400

    adjusted route paths in App.js. Edited text in editDetails.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,9 @@ import PayHistory from './components/PayHistory.js'
 import Expenses from './components/Expenses.js'
 import Details from './components/Details.js'
 import Nav from './components/Nav.js'
-import newDetails from './components/propertyDetails/newDetails'
-import individualDetails from './components/propertyDetails/individualDetails'
+import newDetails from './components/propertyDetails/newDetails.js'
+import individualDetails from './components/propertyDetails/individualDetails.js'
+import editDetails from './components/propertyDetails/editDetails.js'
 
 
 function App() {
@@ -14,12 +15,12 @@ function App() {
       <Router>
       <Nav />
         <Switch>
-          
           <Route exact path="/pay-history" component={PayHistory}/>
           <Route exact path="/property-portfolio" component={Details}/>
           <Route exact path="/property-expenses" component={Expenses}/>
-          <Route exact path="/propertyDetails/newDetails" component={newDetails}/>
-          <Route exact path="/propertyDetails/individualDetails" component={individualDetails}/>
+          <Route exact path="/newdetails" component={newDetails}/>
+          <Route exact path="/individualdetails" component={individualDetails}/>
+          <Route exact path="/editdetails" component={editDetails}/>
         </Switch>
       </Router>
       
--- client/src/components/propertyDetails/editDetails.js
+++ client/src/components/propertyDetails/editDetails.js
@@ -3,9 +3,80 @@ import React, { Component } from 'react'
 export default class editDetails extends Component {
     render() {
         return (
-            <div>
+
+            <div className="container">
+                
+                <div className="container inner-container">
+                <h2>Edit Rental Property One Profile</h2>
+
+                <form>
+                    <div className="row">
+                        <div className="col">
+                        <input type="text" className="form-control" placeholder="First name"></input>
+                        </div>
+                        <div className="col">
+                        <input type="text" className="form-control" placeholder="Last name"></input>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div className="col">
+                        <input type="text" className="form-control" placeholder="Phone Number"></input>
+                        </div>
+                        <div className="col">
+                        <input type="text" className="form-control" placeholder="Email Address"></input>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div className="col">
+                        <input type="text" className="form-control" placeholder="Address"></input>
+                        </div>
+                        <div className="col">
+                        <input type="text" className="form-control" placeholder="City"></input>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div className="col">
+                        <input type="text" className="form-control" placeholder="State"></input>
+                        </div>
+                        <div className="col">
+                        <input type="text" className="form-control" placeholder="Zip Code"></input>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div className="col">
+                        <input type="text" className="form-control" placeholder="Year Built"></input>
+                        </div>
+                        <div className="col">
+                        <input type="text" className="form-control" placeholder="Current Rent"></input>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div className="col">
+                        <input type="date" className="form-control" placeholder="Rent Due Date"></input>
+                        </div>
+                        <div className="col">
+                        <input type="date" className="form-control" placeholder="Last Kitchen Update"></input>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div className="col">
+                        <input type="date" className="form-control" placeholder="Last Roof Upgrade"></input>
+                        </div>
+                        <div className="col">
+                        <input type="date" className="form-control" placeholder="Last Bathroom Upgrade"></input>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleFormControlTextarea1">Notes</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <button class="btn btn-primary" type="submit">Create Profile Now</button>
+                </form>
+
+                </div>
                 
             </div>
+
         )
     }
 }
