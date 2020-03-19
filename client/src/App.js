a0f82788f2896c9cfe355cbb247388efef2ca50f
leer <ladesigns90@gmail.com>
2020-03-18 20:37:10 -0400

    deleted imported components no longer being used.

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,7 @@ import Details from './components/Details.js'
 import Nav from './components/Nav.js'
 import newDetails from './components/propertyDetails/newDetails.js'
 import Footer from './components/Footer.js'
-import individualExpense from './components/propertyExpenses/individualExpense.js'
-import editExpense from './components/propertyExpenses/editExpense.js'
 import newExpense from './components/propertyExpenses/newExpense.js'
-import individualPayHistory from './components/payHistory/individualPayHistory.js'
-
 import newPayHistory from './components/payHistory/newPayHistory.js'
 
 
@@ -24,11 +20,7 @@ function App() {
           <Route exact path="/property-portfolio" component={Details}/>
           <Route exact path="/property-expenses" component={Expenses}/>
           <Route exact path="/newdetails" component={newDetails}/>
-          
-          <Route exact path="/individualexpense" component={individualExpense}/>
-          <Route exact path="/editexpense" component={editExpense}/>
           <Route exact path="/newexpense" component={newExpense}/>
-          <Route exact path="/individualpayhistory" component={individualPayHistory}/>
           <Route exact path="/newpayhistory" component={newPayHistory}/>
         </Switch>
         <Footer />
